25d5ddd5ce2aa954b2f2a851a5b11315cc842d0a
DESKTOP-RJT8CUH\kingw <kingwisdom.dev@gmail.com>
2022-05-01 10:21:18 +0700

    router inside redux saga using history and react router v6

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { CssBaseline, Paper } from '@mui/material';
 import { responsiveFontSizes, StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import './App.css';
 import { THEMES } from './constants';
+import history from './history';
+import I18nProvider from './i18n/Provider';
 import { selectLocale, selectThemeMode } from './redux/userPreferenceSlice';
 import AppRoutes from './routes';
 import { darkTheme, lightTheme } from './themes';
-import I18nProvider from './i18n/Provider';
-import './App.css';
 
 function App() {
   const currentThemeMode = useSelector(selectThemeMode);
@@ -20,14 +21,14 @@ function App() {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={currentTheme}>
-        <BrowserRouter>
+        <HistoryRouter history={history}>
           <CssBaseline />
           <I18nProvider locale={currentLocale} defaultLocale={currentLocale}>
             <Paper elevation={0} square>
               <AppRoutes />
             </Paper>
           </I18nProvider>
-        </BrowserRouter>
+        </HistoryRouter>
       </ThemeProvider>
     </StyledEngineProvider>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import { selectCurrentUser } from '../containers/Auth/authSlice';
 import SignInScreen from '../containers/Auth/SignInScreen';
 import SignUpScreen from '../containers/Auth/SignUpScreen';
 import Counter from '../containers/Counter';
 import TestMUI from '../TestMUI';
+
+function Dashboard() {
+  const location = useLocation();
+
+  return (
+    <div>
+      Dashboard Page
+      <Link to="/dashboard">Dashboard</Link>
+      <hr />
+      <Link to="/dashboard1">Dashboard1</Link>
+      <hr />
+      <Link to="/dashboard2">Dashboard2</Link>
+      <hr />
+      <Link to="/login" state={{ from: location }}>
+        Login
+      </Link>
+      <Outlet />
+    </div>
+  );
+}
 
 function NotFound() {
   return <div>NotFound Page</div>;
@@ -11,6 +33,23 @@ function NotFound() {
 
 function ForgotPassword() {
   return <div>ForgotPassword Page</div>;
+}
+
+function RequireAuth() {
+  const currentUser = useSelector(selectCurrentUser);
+  console.log('currentUser', currentUser);
+  const location = useLocation();
+  console.log('location', location);
+
+  if (!currentUser) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return <Outlet />;
 }
 
 function AppRoutes() {
@@ -23,6 +62,11 @@ function AppRoutes() {
       <Route path="/login" element={<SignInScreen />} />
       <Route path="/create-account" element={<SignUpScreen />} />
       <Route path="/forgot" element={<ForgotPassword />} />
+      <Route element={<RequireAuth />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard1" element={<Dashboard />} />
+        <Route path="/dashboard2" element={<Dashboard />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
