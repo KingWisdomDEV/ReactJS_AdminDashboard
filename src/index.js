aef2fc4d9a71317cb82f4f736e8dca770f6bb1de
DESKTOP-RJT8CUH\kingw <kingwisdom.dev@gmail.com>
2022-05-03 09:48:54 +0700

    add lib redux-toolkit-persist to save state to localstorage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
+import { persistStore } from 'reduxjs-toolkit-persist';
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react';
 import App from './App';
 import store from './redux/store';
+
+const persistor = persistStore(store);
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </ReduxProvider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage'; // defaults to localStorage for web
 import authReducer from '../containers/Auth/authSlice';
 import counterReducer from '../containers/Counter/counterSlice';
 import rootSaga from './rootSaga';
@@ -8,17 +10,30 @@ import userPreferenceReducer from './userPreferenceSlice';
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'userPreference'],
+};
+
+const reducers = combineReducers({
+  // Each ReduxSlide wrap inside each container folder
+  counter: counterReducer,
+  userPreference: userPreferenceReducer,
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 // Create a Redux Store
 export default configureStore({
-  reducer: {
-    // Each ReduxSlide wrap inside each container folder
-    counter: counterReducer,
-    userPreference: userPreferenceReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
-      serializableCheck: true,
+      serializableCheck: {
+        /* ignore persistance actions */
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }).concat(sagaMiddleware),
 });
 
